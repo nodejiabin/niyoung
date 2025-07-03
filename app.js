b6c8658306f04cf5720bd0241ef64e31afced412
wjb434473829 <wangjiabin@huimei.com>
2025-07-03 14:05:42 +0800

    add app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: wjb
+ * @Date: 2025-07-03 14:04:57
+ * @LastEditors: wjb
+ * @LastEditTime: 2025-07-03 14:05:11
+ * @Description: 启动文件
+ */
+export default function App() {
+  return <div>Hello World</div>;
+}
